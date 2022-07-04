a806e8236911336e9cc41904bb28c1c620e9a6d5
Josenilsonfariasx <fariaslwork@gmail.com>
2022-07-04 15:07:28 -0300

    Atualizando dados do js

--- js/index.js
+++ js/index.js
@@ -1,19 +1,18 @@
+//recebendo os valores
+
 var nome = document.getElementById('name')
 var buton = document.getElementById('enviar')
 var input = document.getElementsByName('input')
+var dat = document.getElementById('date')
 
-
-
-
-
+//eventos
 buton.addEventListener('click', enviar)
-       
 
-function enviar(){
-    
+
+function enviar(){  /* Mostra o resultado da avaliação na tela */
+
     var texto = nome.value
-    /* Tentando receber o valor do input e jogar em um alert 
-    document.write(`Passiente ${texto} esta ${recebe}`)*/
+    var date = dat.value
 
     for(i=0; i < input.length; i++)
 
@@ -23,8 +22,8 @@ function enviar(){
     }
 
 
-    alert(`O paciente ${texto} esta ${inp}`)
-    console.log(`${inp}`)
+    alert(`O paciente ${texto} esta ${inp}, data da sua avaliação ${date}`)
+    console.log(`${inp}` + date)
 
 }
 
